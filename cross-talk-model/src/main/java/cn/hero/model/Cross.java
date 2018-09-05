d003c4a87dbcd18de3c97748c6ac553e051dc2af
李杰 <2624824150@qq.com>
2018-09-05 20:05:10 +0800

    Initial commit

---
+++ cross-talk-model/src/main/java/cn/hero/model/Cross.java
@@ -0,0 +1,60 @@
+package cn.hero.model;
+
+import java.util.Date;
+
+/**
+ * cross 实体类
+ * Author: lijie
+ * CreatTime: 2018/9/5
+ */
+public class Cross {
+
+    private Integer id;
+    private String type; //类型
+    private String content;  //内容
+    private String creatBy;  //发表人
+    private Date creatTime;  //发表时间
+
+    public Cross() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCreatBy() {
+        return creatBy;
+    }
+
+    public void setCreatBy(String creatBy) {
+        this.creatBy = creatBy;
+    }
+
+    public Date getCreatTime() {
+        return creatTime;
+    }
+
+    public void setCreatTime(Date creatTime) {
+        this.creatTime = creatTime;
+    }
+}

cd5bec47f13d08116b9d4e0f9867a1a48babd7c0
李杰 <2624824150@qq.com>
2018-09-13 14:31:24 +0800

    sql文件上传

--- tb_user.sql
+++ tb_user.sql
@@ -1,3 +1,7 @@
 INSERT INTO `tb_user` VALUES (1, 'asdf', '111');
 INSERT INTO `tb_user` VALUES (2, '007', '007');
 INSERT INTO `tb_user` VALUES (3, '001', '001');
+INSERT INTO `tb_user` VALUES (4, 'mao', '123');
+INSERT INTO `tb_user` VALUES (5, '002', '002');
+INSERT INTO `tb_user` VALUES (6, '003', '003');
+INSERT INTO `tb_user` VALUES (7, '0', '0');
